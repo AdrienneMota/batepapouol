dd77809f7c9ffc44564830827b8e6b311dfb5f8d
AdrienneMota <adriennemota97@gmail.com>
2022-08-29 02:12:08 -0400

    requisitos implementados mas com falhar

--- script.js
+++ script.js
@@ -4,38 +4,50 @@ const nomeuser = prompt("Bem-vindo(a) ao bate papo uol. Por favor, digite seu li
 
 const usuario = {
     name: ""
-};
+}
+
+usuario.name = nomeuser;
 
 function cadastrarusuario(){
-    usuario.name = nomeuser;
     const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ', usuario);
     requisicao.then(tratarsucesso_user);
     requisicao.catch(tratarerro_user);
 }
 
 function tratarsucesso_user(){
-    setInterval(listarmensagem, 3000);
+    listarmensagem();
 }
 
 function tratarerro_user(erro){
     if(erro.response.status === 400){
-        alert('400.. O nome de usuário já existe, por favor, digite um nome válido');
-        location.reload();
+        alert('400.. O nome de usuário já existe, por favor, digite um nome válido.');
+        window.location.reload();
     }else{
         alert('Ops.. Um erro inesperado. Tente novamente mais tarde.')
     }
 }
 
-cadastrarusuario();
+setInterval(listarmensagem, 3000);
 
-///////////////////////////////////////////////////
+cadastrarusuario();
+//////////////////////////////////////////////////
+//avisando para o servidor que o usuário ainda está na sala
+
+function toAqui(){
+    usuario.name = nomeuser;
+    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status ', usuario);
+}
+
+setInterval(toAqui, 5000);
+
+//////////////////////////////////////////////////
 //renderizando as mensagens na tela
 
 const tipomensagem = {
     private_message: 'private_message',
     status: 'status',
     message_normal: 'message'
-}; // objeto para guardar os tipos de mensagens
+} // objeto para guardar os tipos de mensagens
 
 
 function listarmensagem(){
@@ -53,11 +65,11 @@ function renderizarmensagens(resposta){
             mensagemnormal(mensagem);
         }
         if(mensagem.type === tipomensagem.private_message){
-            //aqui chama o método
+            mensagemprivada(mensagem);
         }
         if(mensagem.type === tipomensagem.status){
             mensagemstatus(mensagem);
-        };
+        }
     }
     const mural = document.querySelector('.mural');
     let lastmensagem = mural.lastElementChild;
@@ -81,8 +93,20 @@ function mensagemnormal(mensagem){
     </div>`;
 }
 
-function mensagemprivada(){
-    //função renderizar mensagem privada
+function mensagemprivada(mensagem){
+    if(mensagem.from === usuario.nome){
+        const listamsg = document.querySelector('.mural');
+        listamsg.innerHTML += 
+        `<div class="mensagem-reservada">
+        <div class="hora">(${mensagem.time})</div>
+        <div class="nome-user"><strong>${mensagem.from}</strong></div>
+        <div class="to">reservadamente para</div>
+        <div class="destino">
+            <strong>${mensagem.to}</strong>
+        </div>
+        <div class="conteudo">${mensagem.text}</div>
+    </div>`;    
+    }
 }
 
 function mensagemstatus(mensagem){
@@ -98,6 +122,35 @@ function mensagemstatus(mensagem){
 }
 
 function mostrarerro(){
+    alert('Ops... Tivemos um erro inesperado.');
+    console.log(erro);
+}
+
+
+//////////////////////////////////////////////////
+//enviar mensagem
+
+function enviarmensagem(){
+    const textomensagem = document.querySelector('.texto');
+
+    const texto = {
+        from: usuario.nome,
+        to: "Todos",
+        text: textomensagem.value,
+        type: "message"
+    }
+
+    const promisse = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', texto);
+    promisse.then(printar);
+    // promisse.catch(usuario_fora);
+}
+
+function printar(response){
+    console.log(response.data)
+}
+
+
+function usuario_fora(){
     alert('erro');
 }
 
